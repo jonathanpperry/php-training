8767bdaca4de3f5bd5439855c26f8d838bfcc220
mc-perry <jpper@umich.edu>
2020-06-30 15:54:58 +0900

    Created a migration for the maintenance table and copied over the project to 8_4

---
+++ test/database/migrations/2020_06_30_063919_create_maintenance_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaintenanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenance', function (Blueprint $table) {
+            $table->time('start')->default(DB::raw('CURRENT_TIMESTAMP'))->Comment('開始時間');
+            $table->time('end')->Comment('終了時間');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenance');
+    }
+}
