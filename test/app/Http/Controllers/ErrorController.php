1ec69beb89279ac399eba19b726873b9dc2a0a35
mc-perry <jpper@umich.edu>
2020-07-01 13:45:03 +0900

    Seem to have finished 8_5 to a more-or-less satisfactory extent

---
+++ test/app/Http/Controllers/ErrorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ErrorController extends Controller
+{
+    /**
+     * Handle an error
+     *
+     * @param string $errorCode
+     * @return JsonResponse
+     */
+
+    public function handleError(string $errorCode)
+    {
+        switch ($errorCode) {
+            case "100010":
+                return ["code" => "100010", "data" => "不正アクセス"];
+            case "100011":
+                return ["code" => "100011", "data" => "ログインエラー"];
+            case "100012":
+                return ["code" => "100012", "data" => "データベースエラー"];
+        }
+    }
+}
